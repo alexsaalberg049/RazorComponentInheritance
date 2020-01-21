1d531c2343c18a6e81dfcf75468c85ab465a13a3
Alex Saalberg <alexsaalberg.propro@gmail.com>
2020-01-21 08:04:52 -0800

    First commit

---
+++ Components/Child.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorComponentInheritance.Components
+{
+    public partial class Child : Parent
+    {
+        public void Exclame()
+        {
+            Word = Word + "!";
+        }
+    }
+}
---
+++ Components/Parent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorComponentInheritance.Components
+{
+    public class Parent : ComponentBase
+    {
+        public string Word;
+    }
+}
